fd58ffff051e7e1f10699399519b3cb1f2dca685
Eshan Imesh <111144332+ImeshR@users.noreply.github.com>
2025-05-01 18:33:02 +0530

    updated public/index added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 // pages/index.js
 import dynamic from "next/dynamic";
 import Head from "next/head";
-import Script from "next/script";
 
-// Dynamically import ARScene component with no SSR
 const ARScene = dynamic(() => import("../components/ARScene"), {
   ssr: false,
 });
@@ -14,20 +12,14 @@ export default function Home() {
       <Head>
         <title>AR.js in Next.js</title>
       </Head>
-       
-
-      {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
-      <Script
-        src="https://aframe.io/releases/0.6.0/aframe.min.js"
-        strategy="beforeInteractive"
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `
+            <script src="https://aframe.io/releases/0.6.0/aframe.min.js"></script>
+            <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
+          `,
+        }}
       />
-      {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
-      <Script
-        src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"
-        strategy="beforeInteractive"
-      />
-
-      {/* Render ARScene component */}
       <ARScene />
     </>
   );
